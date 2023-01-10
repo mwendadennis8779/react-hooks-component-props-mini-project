0fbd3ca0453ba7d2521b0af93810bd3d1c6116ca
Dennis <mwendadennis8779@gmail.com>
2023-01-10 19:21:20 +0300

    Completed assignment

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = ({ image= "https://via.placeholder.com/215", about }) => {
+    return <div>
+        <aside>
+            <img
+                src={image}
+                alt='blog logo'
+            />
+            <p>{about}</p>
+
+        </aside>
+    </div>;
+};
+
+export default About;
